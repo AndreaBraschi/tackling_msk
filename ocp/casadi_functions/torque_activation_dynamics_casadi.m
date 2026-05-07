343e85f9284dfb39de73532bc761df80f2d10189
AndreaBraschi <andreabraschi.11@gmail.com>
2026-05-07 13:25:43 +0100

    created mode for torque activation dynamics

---
+++ ocp/casadi_functions/torque_activation_dynamics_casadi.m
@@ -0,0 +1,12 @@
+function activation_dynamics_function = torque_activation_dynamics_casadi(num_actuators, muscle_model_path)
+import casadi.*
+addpath(muscle_model_path);
+
+excitation  = SX.sym('excitation', num_actuators); % Excitation signals (input to the ODE)
+activation  = SX.sym('activation', num_actuators); % Activations (ODE output)
+
+dadt = torque_activation_dynamics(excitation, activation);
+
+activation_dynamics_function = Function('torque_activation_dynamics_casadi', ...
+    {excitation, activation}, {dadt});
+end
