35d8359064926f0abf446b9e2e6621dc675d85ca
AndreaBraschi <andreabraschi.11@gmail.com>
2026-05-13 13:17:44 +0100

    addition: computation of first and second derivative of the dependent coordinate

--- ocp/kinematic_coupling/cubic.m
+++ ocp/kinematic_coupling/cubic.m
@@ -1,5 +1,14 @@
-function y = cubic(x, a, b, c, d)
+function y = cubic(x, x_vel, x_acc, a, b, c, d)
    
-y = a * x.^3 + b * x.^2 + c * x + d;
+y.pos = a * x.^3 + b * x.^2 + c * x + d;
+
+% first derivative
+y.vel = ((3 * a * x.^2) + (2 * b * x) + c) * x_vel;
+
+% second derivative
+y.acc = ((6 * a * x) + (2 * b)) * x_vel.^2 + ...
+    (((3 * a * x.^2) + (2 * b * x) + c) * x_acc);
+
+
 
 end
--- ocp/kinematic_coupling/linear.m
+++ ocp/kinematic_coupling/linear.m
@@ -1,5 +1,11 @@
-function y = linear(x, a, b)
+function y = linear(x, x_vel, x_acc, a, b)
    
-y = a * x + b;
+y.pos = a * x + b;
+
+% first derivative
+y.vel = a * x_vel;
+
+% second derivative
+y.acc = a * x_acc;
 
 end
