42e24fb9242ee90f7c6234250a174471e2fedab9
AndreaBraschi <andreabraschi.11@gmail.com>
2026-05-20 11:05:31 +0100

    created mode for function that runs OpenSim IK on a single trial

---
+++ experimental/ik/run_single_ik.py
@@ -0,0 +1,24 @@
+from opensim import Model
+from osim_utils import run_ik
+from osim_utils.read import readTrc
+
+from numpy import ndarray
+from pandas import DataFrame
+
+def run_single_ik(model_filepath: str, mot_filepath: str, setup_filepath: str, trc_filepath: str, output_dir: str) -> int:
+
+    # load model
+    model = Model(model_filepath)
+
+    # read from the trc file the initial and final times
+    trc_df: DataFrame = readTrc(trc_filepath)
+    time: ndarray = trc_df['time'].values
+
+    init: float = float(time[0])
+    end: float = float(time[-1])
+
+    run_ik(model, trc_filepath, mot_filepath, setup_filepath, output_dir, init, end)
+
+    return 0
+
+
