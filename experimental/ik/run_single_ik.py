a7f553a3554e08041d2a962c360775c440a1dcc0
AndreaBraschi <andreabraschi.11@gmail.com>
2026-05-20 14:50:17 +0100

    fix: import module

--- experimental/ik/run_single_ik.py
+++ experimental/ik/run_single_ik.py
@@ -1,5 +1,5 @@
 from opensim import Model
-from osim_utils import run_ik
+from osim_utils.tools.run_ik import run_ik
 from osim_utils.read import readTrc
 
 from numpy import ndarray
@@ -12,12 +12,12 @@ def run_single_ik(model_filepath: str, mot_filepath: str, setup_filepath: str, t
 
     # read from the trc file the initial and final times
     trc_df: DataFrame = readTrc(trc_filepath)
-    time: ndarray = trc_df['time'].values
+    time: ndarray = trc_df['Time'].values
 
     init: float = float(time[0])
     end: float = float(time[-1])
 
-    run_ik(model, trc_filepath, mot_filepath, setup_filepath, output_dir, init, end)
+    run_ik(model, trc_filepath, mot_filepath, output_dir, init, end, setup_filepath)
 
     return 0
 
