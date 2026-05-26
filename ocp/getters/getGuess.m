7aa1cd67dfb03e5364e9991d9b617dd6a0e72465
AndreaBraschi <andreabraschi.11@gmail.com>
2026-05-26 22:19:08 +0100

    fix: correct dimensionality

--- ocp/getters/getGuess.m
+++ ocp/getters/getGuess.m
@@ -171,8 +171,8 @@ guess.dFTtilde_col = (dFTtilde_col)./scaling.dFTtilde;
 
 % Torque actuators
 guess.a_a = 0.1 * ones(T_mesh, num_actuators);
-guess.e_a = 0.1 * ones(T_col, num_actuators);
-guess.a_a_col= 0.01 * ones(T_mesh, num_muscles);
+guess.e_a = 0.1 * ones(T_mesh, num_actuators);
+guess.a_a_col= 0.01 * ones(T_col, num_actuators);
 
 % look for invalid numerical values
 validate_guess(guess);
