72d9285e478f0b49eefd717d848342ae74182fda
AndreaBraschi <andreabraschi.11@gmail.com>
2026-05-27 15:28:16 +0100

    add: more rpbust way of constructing path to config file; use of config file to read muscle parameters

--- ocp/casadi_functions/buildForceEquilibriumFunc.m
+++ ocp/casadi_functions/buildForceEquilibriumFunc.m
@@ -1,31 +1,38 @@
-function force_equilibrium = buildForceEquilibriumFunc(muscle_model_path, num_muscles, MTparams)
-
-import casadi.*
-addpath(muscle_model_path);
-
+function force_equilibrium_sym = buildForceEquilibriumFunc(pathMuscleModel, num_muscles, MTparameters)
 % Function for Hill-equilibrium
+import casadi.*
+addpath(pathMuscleModel);
+
+% read config
+json_str = fileread(fullfile(pathMuscleModel, "config.json"));
+config_struct = jsondecode(json_str);
+
+% retrieve parameters of force-length-velocity curves
+Fvparam = config_struct.("force_velocity_length").("Fvparam");
+Fpparam = config_struct.("force_velocity_length").("Fpparam");
+Faparam = config_struct.("force_velocity_length").("Faparam");
+
+% inputs: those variables that will be numerically filled in.
 FTtilde     = SX.sym('FTtilde', num_muscles); % Normalized tendon forces
 a           = SX.sym('a', num_muscles); % Muscle activations
 dFTtilde    = SX.sym('dFTtilde', num_muscles); % Time derivative tendon forces
 lMT         = SX.sym('lMT', num_muscles); % Muscle-tendon lengths
 vMT         = SX.sym('vMT', num_muscles); % Muscle-tendon velocities
+
+% outputs: pre-register dimensionality. These will be the variables that
+% will determine how the inputs are related to one another.
 Hilldiff    = SX(num_muscles, 1); % Hill-equilibrium
 FT          = SX(num_muscles, 1); % Tendon forces
 Fce         = SX(num_muscles, 1); % Contractile element forces
 Fiso        = SX(num_muscles, 1); % Normalized forces from force-length curve
 vMmax       = SX(num_muscles, 1); % Maximum contraction velocities
 
-% Parameters of force-length-velocity curves
-load Fvparam
-load Fpparam
-load Faparam
-
 for m = 1:num_muscles
     [Hilldiff(m), FT(m), Fce(m), Fiso(m), vMmax(m)] = ...
-        ForceEquilibrium_FtildeState_GC(a(m), FTtilde(m), dFTtilde(m),...
-        lMT(m), vMT(m), MTparams(:,m), Fvparam, Fpparam, Faparam);
+        force_equilibrium(a(m), FTtilde(m), dFTtilde(m),...
+        lMT(m), vMT(m), MTparameters(:,m), Fvparam, Fpparam, Faparam);
 end
 
-force_equilibrium = Function('force_equilibrium',{a, FTtilde, dFTtilde, lMT, vMT},...
+force_equilibrium_sym = Function('force_equilibrium', {a, FTtilde, dFTtilde, lMT, vMT},...
     {Hilldiff, FT, Fce, Fiso, vMmax});
 end
